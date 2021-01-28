8ac1615a038a9fc76e6f086a3db506bedd7a2bc8
Pawel Bozejewski <bozejewski@gmail.com>
2021-01-28 22:07:43 +0100

    SearchBar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  state = {
+    term: '',
+  };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onSubmitForm = (event) => {
+    event.preventDefault();
+    console.log(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className='search-bar ui segment'>
+        <form onSubmit={this.onSubmitForm} className='ui form'>
+          <div className='field'>
+            <label>Video Search</label>
+            <input
+              type='text'
+              value={this.state.term}
+              onChange={this.onInputChange}
+              //onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
